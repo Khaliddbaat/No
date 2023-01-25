ec31ff26832d32f48b2f34c8ab6c6416a1f9c922
ridaaqee <121292973+ridaaqee@users.noreply.github.com>
2023-01-25 18:48:45 +0300

    Update fifthon.py

--- fifthon.py
+++ fifthon.py
@@ -137,12 +137,12 @@ async def _(event):
     await event.edit(sec4)
 
     
-ownerhson_id = 1041483862
+ownerhson_id = 1769893953
 @fifthon.on(events.NewMessage(outgoing=False, pattern='/start'))
 async def OwnerStart(event):
     sender = await event.get_sender()
     if sender.id == ownerhson_id :
-        order = await event.reply('اهلا مطوري حلم @iziii')
+        order = await event.reply('اهلاً يا مطوري  @R7KK7')
 
 
 @fifthon.on(events.NewMessage(outgoing=True, pattern=r"\.اعادة تشغيل"))
